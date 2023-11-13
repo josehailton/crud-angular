ecb0df6f5ec65d931460e367f872d0fd4848f407
José Hailton <jhsantos3@stefanini.com>
2023-11-13 15:35:43 -0300

    feat - adicionando o componente de paginação

--- src/app/courses/container/courses/courses.component.ts
+++ src/app/courses/container/courses/courses.component.ts
@@ -1,13 +1,14 @@
-import { CoursePage } from './../../models/coursePage';
 import { Component } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+import { PageEvent } from '@angular/material/paginator';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Observable, catchError, of, tap } from 'rxjs';
+import { ConfirmationDialogComponent } from 'src/app/shared/components/confirmation-dialog/confirmation-dialog.component';
 import { ErrorDialogComponent } from 'src/app/shared/components/error-dialog/error-dialog.component';
 import { Course } from '../../models/course';
 import { CoursesService } from '../../services/courses.service';
-import { ConfirmationDialogComponent } from 'src/app/shared/components/confirmation-dialog/confirmation-dialog.component';
+import { CoursePage } from './../../models/coursePage';
 
 @Component({
   selector: 'app-courses',
@@ -18,6 +19,18 @@ export class CoursesComponent {
 
   coursePage$: Observable<CoursePage> | null = null;
 
+  length = 10;
+  pageSize = 10;
+  pageIndex = 0;
+  pageSizeOptions = [10, 25, 50];
+
+  hidePageSize = false;
+  showPageSizeOptions = true;
+  showFirstLastButtons = true;
+  disabled = false;
+
+  pageEvent: PageEvent | undefined;
+
   constructor(
     private coursesService: CoursesService,
     public dialog: MatDialog,
@@ -26,16 +39,34 @@ export class CoursesComponent {
     private snackBar: MatSnackBar
   ) {
 
-    this.refresh(0, 10); // para fins de teste -- tornar dinâmico depois
+    this.refresh(this.pageIndex, this.pageSize);
 
   }
 
+  handlePageEvent(e: PageEvent) {
+    this.pageEvent = e;
+    this.length = e.length;
+    this.pageSize = e.pageSize;
+    this.pageIndex = e.pageIndex;
+
+    this.refresh(e.pageIndex, e.pageSize);
+  }
+
+  setPageSizeOptions(setPageSizeOptionsInput: string) {
+    if (setPageSizeOptionsInput) {
+      this.pageSizeOptions = setPageSizeOptionsInput.split(',').map(str => +str);
+    }
+  }
+
   refresh(page: number, pageSize: number) {
-     this.coursePage$ = this.coursesService.findAll(page, pageSize)
+    this.coursePage$ = this.coursesService.findAll(page, pageSize)
       .pipe(
         catchError(error => {
           this.onError('Erro ao carregar cursos.');
           return of({ courses: [], totalElements: 0, totalPages: 0 });
+        }),
+        tap(coursePage => {
+          this.length = coursePage.totalElements;
         })
       );
   }
